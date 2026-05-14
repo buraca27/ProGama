41ce9da437eb4fdde1e78a097cbfcca5427359aa
JoaoGomes8 <joaogomes2609@gmail.com>
2026-05-14 11:20:12 +0100

    feat: add 'Modificar Landing Page' menu item to admin section

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -229,6 +229,10 @@ export default function AuthenticatedLayout({
                                     id="definicoes"
                                     label="Definições do Sistema"
                                 />
+                                <MenuButton
+                                    id="updateLandingPage"
+                                    label="Modificar Landing Page"
+                                />
                             </>
                         )}
 
